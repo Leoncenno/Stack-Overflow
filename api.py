e20b029dea5e0d20690f76057692d7b4c5e23810
Leonard Odongo <leonarddng@gmail.com>
2022-02-22 15:42:30 +0300

    API end point for new user registration

--- api.py
+++ api.py
@@ -1,9 +1,12 @@
 from crypt import methods
+from enum import unique
 from xml.etree.ElementTree import QName
 from flask import Flask, request, jsonify, json
 from dbconnect import DbConnect
 from flasgger import Swagger
 from flasgger.utils import swag_from
+from validations import Validation
+from werkzeug.security import generate_password_hash
 
 
 app = Flask(__name__)
@@ -11,20 +14,24 @@ app.config["DEBUG"] = True
 swagger = Swagger(app)
 
 db = DbConnect()
+val = Validation()
+
 
 @app.route('/api/v1/questions', methods=['GET'])
 @swag_from('api_docs/getallquestion.yml')
 def all_questions():
     questions = db.get_all_questions()
-    return jsonify(questions)
+    return jsonify(questions), 200
 
 
 @app.route('/api/v1/questions', methods=['POST'])
 @swag_from('api_docs/post_question.yml')
 def post_question():
-    new_question = request.json
-    qn = db.post_a_question(new_question['question'], new_question['description'])
-    return jsonify(qn)
+    new_q = request.json
+    new_question = val.validate(new_q)
+    qn = db.post_a_question(
+        new_question['question'], new_question['description'])
+    return jsonify(qn), 201
 
 
 @app.route('/api/v1/questions/<id>', methods=['PUT'])
@@ -32,21 +39,21 @@ def post_question():
 def update_question(id):
     new_question = request.json
     updated_question = db.update_a_question(id, new_question['question'],)
-    return jsonify(updated_question)
+    return jsonify(updated_question), 201
 
 
 @app.route('/api/v1/questions/<id>', methods=['GET'])
 @swag_from('api_docs/one_question.yml')
 def one_question(id):
     question = db.get_one_question(id)
-    return jsonify(question)
+    return jsonify(question), 200
 
 
 @app.route('/api/v1/questions/<id>/answers', methods=['GET'])
 @swag_from('api_docs/allanswers.yml')
 def answers(id):
     answers = db.get_all_answers(id)
-    return jsonify(answers)
+    return jsonify(answers), 200
 
 
 @app.route('/api/v1/questions/<id>/answers', methods=['POST'])
@@ -54,7 +61,24 @@ def answers(id):
 def post_answer(id):
     new_answer = request.json
     answer = db.post_an_answer(id, new_answer['answer'])
-    return jsonify(answer)
+    return jsonify(answer), 201
+
+
+@app.route('/register', methods=['POST'])
+@swag_from('api_docs/create_new_user.yml')
+def create_new_user():
+    details = request.json
+    password_1 = details['password1']
+    password_2 = details['password2']
+    user_name = details['username']
+    Email = details['email']
+    if password_1 == password_2:
+        password_1 = generate_password_hash(details['password1'])
+        db.sign_up(details['firstname'], details['lastname'], Email,
+                            password_1, password_2, details['dateofbirth'], user_name)
+        return jsonify('Account registered succesfully'), 201
+    else:
+        return jsonify('Conflicting passwords, please enter and confirm the same password!'), 409
 
 
 app.run()
--- dbconnect.py
+++ dbconnect.py
@@ -1,34 +1,38 @@
+from enum import unique
 import psycopg2.extras
 import psycopg2
 import config
 from decouple import config
 
+
 dbhost = config('HOST')
 dbdatabase = config('DATABASE')
-dbuser = config ('USER')
+dbuser = config('USER')
 dbpassword = config('PASSWORD')
 
-class DbConnect(): #connect to the StackOverflow database
+
+class DbConnect():  # connect to the StackOverflow database
     def __init__(self):
         """Connect to the StackOverflow Database Server"""
-        
+
         try:
             self.conn = None
-            params = config #read connection parameters
+            params = config  # read connection parameters
 
-            print('connecting to StackOverflow Database...') #connect to StackOverflow server
+            # connect to StackOverflow server
+            print('connecting to StackOverflow Database...')
             self.conn = psycopg2.connect(
                 host=dbhost,
                 database=dbdatabase,
                 user=dbuser,
                 password=dbpassword)
 
-            self.cur = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) #create a cursor
+            self.cur = self.conn.cursor(
+                cursor_factory=psycopg2.extras.RealDictCursor)  # create a cursor
             self.conn.autocommit = True
-        
+
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
-
 
     def get_all_questions(self):
         self.cur.execute('SELECT * FROM questions')
@@ -36,20 +40,24 @@ class DbConnect(): #connect to the StackOverflow database
         return all_questions
 
     def post_a_question(self, question, description):
-        self.cur.execute('INSERT INTO questions (question, description) VALUES (%s, %s)', (question, description))
+        self.cur.execute(
+            'INSERT INTO questions (question, description) VALUES (%s, %s)', (question, description))
         return {'question': question, 'description': description}
 
     def update_a_question(self, question_id, question):
-        self.cur.execute('SELECT * FROM questions WHERE question_id = %s', question_id)
+        self.cur.execute(
+            'SELECT * FROM questions WHERE question_id = %s', question_id)
         chk = self.cur.fetchone()
         if chk != None:
-            self.cur.execute('UPDATE questions SET question = %s WHERE question_id = %s', (question, question_id))
+            self.cur.execute(
+                'UPDATE questions SET question = %s WHERE question_id = %s', (question, question_id))
             return {'question': question}
         else:
             return 'Question doesnt exist!'
 
     def get_one_question(self, question_id):
-        self.cur.execute('SELECT * FROM questions WHERE question_id = %s', question_id)
+        self.cur.execute(
+            'SELECT * FROM questions WHERE question_id = %s', question_id)
         one_question = self.cur.fetchone()
         if one_question != None:
             return one_question
@@ -57,7 +65,8 @@ class DbConnect(): #connect to the StackOverflow database
             return 'Question doesnt exist!'
 
     def get_all_answers(self, question_id):
-        self.cur.execute('SELECT * FROM answers WHERE question_id = %s', question_id)
+        self.cur.execute(
+            'SELECT * FROM answers WHERE question_id = %s', question_id)
         answers = self.cur.fetchall()
         if answers != None:
             return answers
@@ -65,8 +74,14 @@ class DbConnect(): #connect to the StackOverflow database
             return 'No answers for this question!'
 
     def post_an_answer(self, question_id, answer):
-        self.cur.execute('SELECT * FROM questions WHERE question_id = %s', question_id)
+        self.cur.execute(
+            'SELECT * FROM questions WHERE question_id = %s', question_id)
         chk = self.cur.fetchone()
         if chk != None:
-            self.cur.execute('INSERT INTO answers (question_id, answer) VALUES (%s, %s)', (question_id, answer))
+            self.cur.execute(
+                'INSERT INTO answers (question_id, answer) VALUES (%s, %s)', (question_id, answer))
             return answer
+
+    def sign_up(self, firstname, lastname, email, password1, password2, dateofbirth, username):
+        self.cur.execute('INSERT INTO user_details (first_name, last_name, email, password, confirm_password, date_of_birth, user_name) VALUES (%s, %s, %s, %s, %s, %s, %s)',
+                         (firstname, lastname, email, password1, password2, dateofbirth, username))
